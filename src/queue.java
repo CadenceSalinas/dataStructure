373daef1f70f1d15224797cf1bcf421bec2dece6
Cadence <cadence242@roadrunner.com>
2022-02-28 08:30:03 -0800

    Initial commit

---
+++ src/queue.java
@@ -0,0 +1,70 @@
+/*
+First in, first out - insert at end remove at beginning
+Includes insert, delete, looking
+    - able to insert at tail and delete at head
+
+
+*/
+
+public class queue
+{
+    //variables
+    private Node head;
+
+    //constructor
+    public queue()
+    {
+        head = null;
+    }
+
+    public void add(int newData)
+    {
+        head = new Node(newData, head);
+    }
+
+    public void showList()
+    {
+        Node position = head;
+        while(position != null)
+        {
+            System.out.println(position.getData());
+            position = position.getNextNode();
+        }
+    }
+
+    private class Node
+    {
+        public int data;
+        private Node link;
+
+        //constructor
+        public Node(int data, Node next)
+        {
+            this.data = 0;
+            this.link = null;
+        }
+
+        public void setNextNode(Node node)
+        {
+            this.link = node;
+        }
+
+        public Node getNextNode()
+        {
+            return this.link;
+        }
+
+        public int getData()
+        {
+            return this.data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", link=" + link +
+                    '}';
+        }
+    }//end node
+}//end queue
---
+++ src/queueDriver.java
@@ -0,0 +1,8 @@
+public class queueDriver {
+    public static void main(String[] args)
+    {
+        queue newQueue = new queue();
+        newQueue.add(4);
+        newQueue.showList();
+    }
+}

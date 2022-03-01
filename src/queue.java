c292458751eb2ba3787a85b4fc588297fe710121
Cadence <cadence242@roadrunner.com>
2022-03-01 08:25:24 -0800

    changed to add to list from tail

--- src/queue.java
+++ src/queue.java
@@ -10,21 +10,36 @@ public class queue
 {
     //variables
     private Node head;
+    private Node tail;
 
     //constructor
     public queue()
     {
         head = null;
+        tail = null;
     }
 
     public void add(int newData)
     {
-        head = new Node(newData, head);
+        tail = new Node(newData, tail);
+    }
+
+    public void remove()
+    {
+        if(head != null)
+        {
+            head = head.getNextNode();
+        }
+        else
+        {
+            System.out.println("You're deleting from an empty list.");
+            System.exit(0);
+        }
     }
 
     public void showList()
     {
-        Node position = head;
+        Node position = tail;
         while(position != null)
         {
             System.out.println(position.getData());
